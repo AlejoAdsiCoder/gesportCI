b1bb8348efeee4d49fbaa360c314267de0b50266
Alejok <Alejok>
2019-03-18 00:29:53 -0500

    Proyecto en CI

---
+++ application/controllers/Deportista.php
@@ -0,0 +1,17 @@
+<?php
+class Deportista extends CI_Controller {
+
+
+        public function index()
+        {
+            // $data["entrenador_data"] = $this->mempresa->obtener_datos();
+            $this->carga_layout("lista_deportistas");
+        }
+
+        public function carga_layout() 
+        {
+            $this->load->view('header');
+            $this->load->view('nav');
+            $this->load->view($template);
+        }
+}
