b4b90a72896db78d0a2e23f2cb7dc0ba3dcd4e34
Alejo <apatino@seedem.co>
2019-06-09 22:42:55 -0500

    Añadiendo estilos para el login

--- application/views/login.php
+++ application/views/login.php
@@ -1,4 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/login.css">
+</head>
+<body>
 <div class="container">
+<div class="header-main">
+  
 <form method="post">
   <div class="form-group">
     <label for="exampleInputEmail1">Cedula</label>
@@ -16,7 +28,7 @@
     <div id='msg'> </div>  
     </div></div></div>  
 </form>
-
+</div>
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
@@ -57,3 +69,4 @@
         });  
         });  
         </script>   
+</body>
